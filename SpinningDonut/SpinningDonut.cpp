9e1cd3989b3b2ccfe3e4c526a29aca0629fe3638
Batox <batox369@gmail.com>
2026-03-31 10:58:25 -0500

    Version Final

--- SpinningDonut/SpinningDonut.cpp
+++ SpinningDonut/SpinningDonut.cpp
@@ -3,22 +3,20 @@
 #include <chrono>
 #include <thread>
 
-
 using namespace std;
 
 int main() {
 
-    const int R1 = 10, R2 = 3, FOV = 10, DISTANCE = 15;
+    const int R1 = 10, R2 = 3, FOV = 40, DISTANCE = 50;
     const int width = 90, height = 26;
-    const char DONUT_CHARS[] = {'.', ',', '-', '~', ':', ';', '#'};
+    const char DONUT_CHARS[] = { '.', ',', '-', '~', ':', ';', '=', '#' };
     char screen[width][height];
-	float zBuffer[width][height];
+    float zBuffer[width][height];
 
     float theta, phi;
 
-    // RotX
-
-    float rotX = 0, rotY = 0;
+    // RotX y RotZ
+    float rotX = 0, rotZ = 0;
 
     system("cls");
 
@@ -27,94 +25,88 @@ int main() {
 
     while (true)
     {
-		rotX += 0.0;
-		rotY += 0.3;
+        rotX += 0.04;
+        rotZ += 0.02;
 
         printf("\x1b[H");
 
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
                 screen[x][y] = ' ';
-				zBuffer[x][y] = 0;
+                zBuffer[x][y] = 0;
             }
         }
 
         for (phi = 0; phi < 6.28; phi += 0.02) {
             for (theta = 0; theta < 6.28; theta += 0.02) {
 
-                // -- The parametric equation of the donut is
-
-                // X = (R + r*cos(theta)) * cos(phi)
-                // Y = (R + r*cos(theta)) * sen(phi)
-                // Z = r*sen(theta)
-
-                float X = (R1 + R2 * cos(theta)) * cos(phi); // Need to find his min and max values to scale it to the screen
+                float X = (R1 + R2 * cos(theta)) * cos(phi);
                 float Y = (R1 + R2 * cos(theta)) * sin(phi);
                 float Z = R2 * sin(theta);
 
-				float rotatedX = X;
-				float rotatedY = cos(rotX) * Y - sin(rotX) * Z;
-				float rotatedZ = sin(rotX) * Y + cos(rotX) * Z;
+                // --- ROTACIÓN X (Local) ---
+                float rotatedX = X;
+                float rotatedY = cos(rotX) * Y - sin(rotX) * Z;
+                float rotatedZ = sin(rotX) * Y + cos(rotX) * Z;
 
-				float tempRotatedX = rotatedX;
-
-                rotatedX = cos(rotY) * tempRotatedX + sin(rotY) * rotatedZ;
-				rotatedY = rotatedY;
-				rotatedZ = -sin(rotY) * tempRotatedX + cos(rotY) * rotatedZ;
+                // --- ROTACIÓN Z (Global) ---
+                float tempRotatedX = rotatedX;
+                rotatedX = cos(rotZ) * tempRotatedX - sin(rotZ) * rotatedY;
+                rotatedY = sin(rotZ) * tempRotatedX + cos(rotZ) * rotatedY;
+                rotatedZ = rotatedZ; // El eje Z no cambia en rotación Z
 
                 X = rotatedX;
                 Y = rotatedY;
-				Z = rotatedZ;
+                Z = rotatedZ;
 
                 Z += DISTANCE;
 
-				float ooz = 1 / Z; // "One over Z" is used to calculate the depth of the point and to scale the projection
+                float ooz = 1 / Z;
 
-                float projX = (X * FOV) / Z; // Max value when X = R1 + R2, Z = R2 + FOV |  Min value when X = -(R1 + R2), Z = -R2 + FOV 
-                float projY = (Y * FOV) / Z; // Max value when Y = R1 + R2, Z = R2 + FOV |  Min value when Y = -(R1 + R2), Z = -R2 + FOV
+                float projX = (X * FOV) / Z;
+                float projY = (Y * FOV) / Z;
 
                 float maxProj = (R1 + R2) * FOV / (FOV - R2);
 
                 float scaleX = (width / 2.0) / maxProj;
                 float scaleY = (height / 2.0) / maxProj;
 
-                int screenX = static_cast<int>((projX * scaleX + width / 2) / 1.5);
+                int screenX = static_cast<int>(((projX * scaleX / 1.5) + width / 2));
                 int screenY = static_cast<int>(projY * scaleY + height / 2);
 
-                // Calculating the normal vector of the surface at the point (theta, phi) and adding light
-
+                // --- NORMALES ---
                 float nX = cos(theta) * cos(phi);
                 float nY = cos(theta) * sin(phi);
                 float nZ = sin(theta);
 
-				float rotatedNX = nX;
-				float rotatedNY = cos(rotX) * nY - sin(rotX) * nZ;
-				float rotatedNZ = sin(rotX) * nY + cos(rotX) * nZ;
+                // Rotación X en Normal
+                float rotatedNX = nX;
+                float rotatedNY = cos(rotX) * nY - sin(rotX) * nZ;
+                float rotatedNZ = sin(rotX) * nY + cos(rotX) * nZ;
 
+                // Rotación Z en Normal
                 float tempRotatedNX = rotatedNX;
+                rotatedNX = cos(rotZ) * tempRotatedNX - sin(rotZ) * rotatedNY;
+                rotatedNY = sin(rotZ) * tempRotatedNX + cos(rotZ) * rotatedNY;
+                rotatedNZ = rotatedNZ;
 
-				rotatedNX = cos(rotY) * tempRotatedNX + sin(rotY) * rotatedNZ;
-				rotatedNY = rotatedNY;
-				rotatedNZ = -sin(rotY) * tempRotatedNX + cos(rotY) * rotatedNZ;
+                nX = rotatedNX;
+                nY = rotatedNY;
+                nZ = rotatedNZ;
 
-				nX = rotatedNX;
-				nY = rotatedNY;
-				nZ = rotatedNZ;
-
-                // Were assuming light (0,0,-1)
-                float lightEmision = -nZ * 7;
+                float lightEmision = -nZ * 8;
 
                 if (screenX >= 0 && screenX < width && screenY >= 0 && screenY < height) {
                     if (ooz > zBuffer[screenX][screenY]) {
                         zBuffer[screenX][screenY] = ooz;
                         if (lightEmision > 0) {
                             int charIndex = static_cast<int>(lightEmision);
+                            if (charIndex > 7) charIndex = 7; // Evitar que rompa si el índice sube
                             screen[screenX][screenY] = DONUT_CHARS[charIndex];
                         }
                     }
                 }
             }
-
         }
 
         for (int y = 0; y < height; y++) {
@@ -124,7 +116,7 @@ int main() {
             cout << endl;
         }
 
-		this_thread::sleep_for(chrono::milliseconds(20));
+        this_thread::sleep_for(chrono::milliseconds(20));
     }
 
     return 0;
